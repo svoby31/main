6010c3bd31cb80d5e9f43b39ae02619350145b29
svoby31 <oliver.svoboda31@gmail.com>
2025-04-07 14:47:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function bookAppointment() {
   const name = document.getElementById("name").value;
   const phone = document.getElementById("phone").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
   const list = document.getElementById("appointmentList");
 
-  if (!name || !phone) return;
-
-  const date = new Date();
-  const formattedDate = date.toISOString().split("T")[0];
+  if (!name || !phone || !date || !time) return;
 
   const li = document.createElement("li");
-  li.textContent = `${name} – ${phone} (${formattedDate})`;
+  li.textContent = `${name} – ${phone} (${date} v ${time})`;
 
   list.appendChild(li);
 
   document.getElementById("name").value = "";
   document.getElementById("phone").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("time").value = "";
 }
